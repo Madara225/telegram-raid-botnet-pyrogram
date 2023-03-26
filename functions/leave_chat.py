868a92051fb7ed80045c91f159208fd61e7b04b9
Madara225 <70082455+Madara225@users.noreply.github.com>
2023-03-26 15:22:50 +0000

    fix output

--- functions/leave_chat.py
+++ functions/leave_chat.py
@@ -15,7 +15,7 @@ class Leavechat(SettingsFunction):
     """Leave the chat"""
 
     def __init__(self, sessions):
-        if console.input("[bold red]are you sure? (y/n): ") == "y":
+        if console.input("[bold red]Are you sure? (y/n): ") == "y":
             asyncio.get_event_loop().run_until_complete(
                 asyncio.gather(*[
                     self.leavechats(session)
@@ -30,16 +30,17 @@ class Leavechat(SettingsFunction):
         try:
             async for dialog in session.get_dialogs():
                 await session.leave_chat(dialog.chat.id, delete=True)
+
                 console.log(
                     "[bold green]Name: {name} id {id}"
-                    .format(name=dialog.chat.first_name, id=dialog.chat.id)
+                    .format(name=dialog.chat.title if dialog.chat.first_name is None else dialog.chat.first_name, id=dialog.chat.id)
                 )
 
         except PeerIdInvalid:
             ...
 
         except ChannelPrivate as private_error:
-            console.print(private_error)
+            ...
 
         except Exception as error:
             console.print("Error : {}".format(error), style="bold white")
